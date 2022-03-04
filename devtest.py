d3530fb1edc14f7c0bafa8051d00b473ad1fd02c
Jeleniozaur <75537787+Jeleniozaur@users.noreply.github.com>
2022-03-04 02:07:49 +0100

    Add files via upload

---
+++ devtest.py
@@ -0,0 +1,71 @@
+import sys
+import os
+import os.path
+import zipfile
+import datetime
+
+def InvalidArgvException():
+    print("Arguments invalid!")
+    print("Available arguments are:\n1.Zip filename\n2.Content to be added to VERSION.txt.\n3.Optional - set it to 't' to update updated.txt with current date.")
+    sys.exit()
+
+def sh(cmd):
+    os.system(cmd)
+
+def CreateTempFileAndZipIt(filename, content, archive):
+    sh("echo " + content + " > /tmp/"+filename)
+    sh("zip -q -u -j " + archive + " /tmp/"+filename)
+    sh("rm /tmp/"+filename)
+    print('created ' + filename + ' and added to ' + archive)
+
+def AppendToZip(filename, content, archive):
+    #check if file exists in zip if yes then unzip
+    zp = zipfile.ZipFile(archive)
+    if filename in zp.namelist():
+        sh("unzip -q -p " + archive + " " + filename + " > /tmp/"+filename)
+        sh("echo " + content + " >> /tmp/"+filename)
+        print('appended ' + filename + ' to ' + archive)
+    else:
+        sh("echo " + content + " > /tmp/"+filename)
+        print('created ' + filename + ' and added to ' + archive)
+
+    sh("zip -q -u -j " + archive + " /tmp/"+filename)
+    sh("rm /tmp/"+filename)
+
+def DoStuff():
+    # Get parameters
+    z = sys.argv[1]  # zip filename
+    u = sys.argv[2]  # update for VERSION.txt
+    o = ''           # optional - assigned later on in the script
+
+    # Check for unused arguments
+    if len(sys.argv) >= 4:
+        o = sys.argv[3]  # optional, for updating updated.txt, must be set to 't' to work
+    if len(sys.argv) > 4:
+        print("More than 3 arguments were specified, they are going to be ignored")
+
+    # Check for argv syntax
+    if not z.endswith('.zip'):
+        print("Please provide a whole filename such as " + z + '.zip')
+        exit()
+    if len(sys.argv) >= 4 and o != 't':
+        print("Please use 't' for this argument to be used")
+        exit()
+
+    # If all checks passed:
+    # Check if zip file already exists
+    if os.path.isfile(z):
+        print(z + ' found')
+        AppendToZip("VERSION.txt", u, z)
+        if o == 't':
+            AppendToZip("updated.txt", str(datetime.datetime.now()), z)
+    else:
+        print(z + ' not found')
+        CreateTempFileAndZipIt("VERSION.txt", u, z)
+        if o == 't':
+            CreateTempFileAndZipIt("updated.txt", str(datetime.datetime.now()), z)
+
+if len(sys.argv) <= 2:
+    InvalidArgvException()
+else:
+    DoStuff()
